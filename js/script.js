51c7450d517a5c0f46720fbbb5137b9e35ce1f43
Stefano-Para <stefano.zina@gmail.com>
2021-05-18 16:32:01 +0200

    base css html

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 // Bonus:
 // Gestire il cambio dell'immagine al click sui pallini in basso (nav);
 // Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
+
+// img 1) 
+// https://wallpaperaccess.com/full/31189.jpg
+
+// img 2)
+// https://i.pinimg.com/originals/5e/65/20/5e6520289b44e11a9e74363c18ce3ee1.jpg
+
+// img 3)
+// https://i.pinimg.com/originals/f4/c7/3b/f4c73b9f323254d5bb4206aa95a62b59.jpg
+
+// img 4)
+// https://wallpapercave.com/wp/wp2033303.jpg
+
+// img 5)
+// https://3.bp.blogspot.com/-eilZTdgbWPA/XFUltCS4Z1I/AAAAAAAABz0/feDxTa3Emtsb3Wx4xxu0hWnFrohqtwQfwCKgBGAs/w1920-h1080-c/mountain-lake-scenery-nature-cottage-25-4K.jpg
+
+var app = new Vue({
+    el: '#root',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
