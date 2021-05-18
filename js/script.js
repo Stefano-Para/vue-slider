07aced2d49bd2dab4b76c04817edcf37a304520f
Stefano-Para <stefano.zina@gmail.com>
2021-05-18 15:31:38 +0200

    link

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Descrizione:
+// Rifare l'esercizio dello slider come fatto assieme in classe.
+// Bonus:
+// Gestire il cambio dell'immagine al click sui pallini in basso (nav);
+// Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente.
