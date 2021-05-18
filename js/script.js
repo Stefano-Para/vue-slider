ab91a382db08af2c165711dd68a6e975815e1417
Stefano-Para <stefano.zina@gmail.com>
2021-05-18 17:29:18 +0200

    esercizio quasi finito, manca setInterval

--- js/script.js
+++ js/script.js
@@ -19,9 +19,41 @@
 // img 5)
 // https://3.bp.blogspot.com/-eilZTdgbWPA/XFUltCS4Z1I/AAAAAAAABz0/feDxTa3Emtsb3Wx4xxu0hWnFrohqtwQfwCKgBGAs/w1920-h1080-c/mountain-lake-scenery-nature-cottage-25-4K.jpg
 
-var app = new Vue({
-    el: '#root',
-    data: {
-      message: 'Hello Vue!'
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            images: [
+
+                "https://wallpaperaccess.com/full/31189.jpg",
+
+                "https://3.bp.blogspot.com/-eilZTdgbWPA/XFUltCS4Z1I/AAAAAAAABz0/feDxTa3Emtsb3Wx4xxu0hWnFrohqtwQfwCKgBGAs/w1920-h1080-c/mountain-lake-scenery-nature-cottage-25-4K.jpg",
+
+                "https://i.pinimg.com/originals/5e/65/20/5e6520289b44e11a9e74363c18ce3ee1.jpg",
+
+                "https://i.pinimg.com/originals/f4/c7/3b/f4c73b9f323254d5bb4206aa95a62b59.jpg",
+
+                "https://wallpapercave.com/wp/wp2033303.jpg"                
+            ],
+            imageIndex: 0,
+        }, // chiusura data
+
+        methods: {
+            nextImage: function () {
+                this.imageIndex++;
+                if (this.imageIndex == this.images.length) {
+                    this.imageIndex = 0;
+                }
+            },
+            prevImage: function () {
+                this.imageIndex--;
+                if (this.imageIndex <= 0) {
+                    this.imageIndex = this.images.length - 1;
+                }
+            }
+        }  // chiusura methods 
+        
     }
-  })
+);
+
+    
